29f5759db8ce18513418d6fb049fa417911903fc
Fluxtev23 <97816591+Fluxtev23@users.noreply.github.com>
2023-10-22 01:19:19 -0400

    alejandro fixy

--- src/app/client/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { GroupProvider } from "./group"
-
-export default function GroupLayout({ children }: {
-    children: React.ReactNode
-}) {
-    return (
-        <GroupProvider>{children}</GroupProvider>
-    )
-}
