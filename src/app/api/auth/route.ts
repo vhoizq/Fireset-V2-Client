bb3cd4d1f40fb9cf15aec1f11fa2eb20951a82cc
Fluxtev23 <97816591+Fluxtev23@users.noreply.github.com>
2023-10-23 18:52:24 -0400

    E

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 
 const ROBLOX_APPID = "2682885889385043103"
 
-const HOST = "http://localhost:3000"
+const HOST = "https://fireset-v2-client.vercel.app"
 
 export const GET = () => {
     return new Response(
         JSON.stringify({
-            data: `https://apis.roblox.com/oauth/v1/authorize?client_id=2682885889385043103&redirect_uri=http://localhost:3000/auth/redirect&scope=openid+profile&response_type=Code&prompts=login+consent&nonce=12345&state=6789`
+            data: `https://apis.roblox.com/oauth/v1/authorize?client_id=2682885889385043103&redirect_uri=https://fireset-v2-client.vercel.app/auth/redirect&scope=openid+profile&response_type=Code&prompts=login+consent&nonce=12345&state=6789`
         }),
         { status: 200 }
     )
